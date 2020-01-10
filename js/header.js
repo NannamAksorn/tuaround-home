ddf6f5349adb1a82a0a258f9758c00ea54826f20
mnphmnmn <nam_nam8@hotmail.com>
2020-01-10 18:19:15 +0700

    animate

--- js/header.js
+++ js/header.js
@@ -1,3 +1,18 @@
+const nav = document.getElementById("nav");
+nav.innerHTML = `
+  <div class="brand">
+    <span class="brand-title-red">TU</span> <span class="brand-title-yellow">AROUND</span>
+  </div>
+  <div class="nav-container" id="navbar">
+    <a class="nav-item  closebtn" href="javascrip:vold(0)" onclick="closeNav()">&times;</a>
+    <a class="nav-item" href="/">Home</a>
+    <a class="nav-item" href="/about">About</a>
+    <a class="nav-item" href="/article">Article</a>
+    <a class="nav-item" href="/faq">FAQ</a>
+    <a class="nav-item" href="/contact">Contact</a>
+  </div>
+  <button class="openbtn" onclick="openNav()">&#9776;</button>
+`
 const navbar = document.getElementById("navbar");
 function openNav() {
   anime({
@@ -13,7 +28,7 @@ function closeNav() {
   anime({
     targets: navbar,
     translateY: '-100%',
-    opacity: 0,
+    opacity: 1,
     easing: 'easeInOutQuad',
     duration: 300
   })
--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
+const mapImg = document.getElementById('map-img');
+const sjBtn = document.getElementById('sj-btn');
 anime({
-  targets: '.map-img',
+  targets: mapImg,
   translateY: -15,
-  easing: 'easeInOutSine'
+  opacity: [0, 1],
+  easing: 'easeInOutQuad'
 });
+// scroll effect
+const mapImg__scrollOut = anime({
+  targets: mapImg,
+  autoplay: false,
+  scale: 0,
+  translateY: -100,
+  easing: 'easeInOutQuad'
+})
+
+const scrollPercent = () => {
+  const bodyST = document.body.scrollTop;
+  const docST = document.documentElement.scrollTop;
+  const docSH = document.documentElement.scrollHeight;
+  const docCH = document.documentElement.clientHeight;
+  
+  
+  return (docST + bodyST) / (docSH - docCH) * 100
+}
+
+window.onscroll = () => {
+  mapImg__scrollOut.seek((scrollPercent() / 100) * mapImg__scrollOut.duration);
+};
+
+// hover effect
+
+function enterButton() { 
+  anime.remove(sjBtn);
+  anime.remove(mapImg);
+  anime({ targets: sjBtn, scale: 1.1, duration: 500 })
+  anime({ targets: mapImg, scale: 1.1, opacity: 1, duration: 500 })
+};
+function leaveButton() { 
+  anime.remove(sjBtn);
+  anime.remove(mapImg);
+  anime({ targets: sjBtn, scale: 1, duration: 500 })
+  anime({ targets: mapImg, scale: 1, duration: 500 })
+};
+
+sjBtn.addEventListener('mouseenter', enterButton, false);
+sjBtn.addEventListener('mouseleave', leaveButton, false);
+
+ScrollOut();
